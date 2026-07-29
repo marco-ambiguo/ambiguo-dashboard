28be66084d46da90542e45df57adb2a37f4fb0df
Marco Battaini <marco@Marcos-MacBook-Pro.local>
2026-07-29 11:00:00 +0200

    Update Redis env variables

--- api/data.js
+++ api/data.js
@@ -1,8 +1,12 @@
 import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url:
+    process.env.UPSTASH_REDIS_REST_KV_REST_API_URL ||
+    process.env.UPSTASH_REDIS_REST_URL,
+  token:
+    process.env.UPSTASH_REDIS_REST_KV_REST_API_TOKEN ||
+    process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
 const DB_KEY = "ambiguo:database:v1";
